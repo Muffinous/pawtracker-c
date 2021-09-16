b996ee6b738c5ccbcfc9f71909afb6e7d2fa4110
Muffinous <mimiafsx@outlook.es>
2021-09-16 20:57:34 +0200

    calendar

--- src/app/home/cal-modal/cal-modal.page.ts
+++ src/app/home/cal-modal/cal-modal.page.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-cal-modal',
   templateUrl: './cal-modal.page.html',
   styleUrls: ['./cal-modal.page.scss'],
 })
-export class CalModalPage implements OnInit {
+export class CalModalPage implements AfterViewInit {
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
+  viewTitle: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  event = {
+    title: '',
+    desc: '',
+    day: 1,
+    month: 'September',
+    year: 2020,
+    startTime: null,
+    endTime: '',
+    allDay: true
+  };
+ 
+  modalReady = false;
+ 
+  constructor(private modalCtrl: ModalController) { }
+ 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.modalReady = true;      
+    }, 0);
   }
-
+ 
+  save() {    
+    this.modalCtrl.dismiss({event: this.event})
+  }
+ 
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+ 
+  onTimeSelected(ev) {    
+    this.event.startTime = new Date(ev.selectedTime);
+  }
+ 
+  close() {
+    this.modalCtrl.dismiss();
+  }
 }
