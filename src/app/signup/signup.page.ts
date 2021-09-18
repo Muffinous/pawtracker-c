a05b0aadccdf3796e1b338c7d79cf598cf2b57e8
Muffinous <mimiafsx@outlook.es>
2021-09-18 13:17:42 +0200

    new buddy on progress

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, ContentChild, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonInput } from '@ionic/angular';
 
@@ -9,25 +10,66 @@ import { IonInput } from '@ionic/angular';
 })
 export class SignupPage implements OnInit {
   showPassword = false;
+  showConfPassword = false;
   passwordToggleIcon;
+  passwordToggleIcon2;
   username: string;
   password: string;
-
+  name: string;
+  surname: string;
+  email: string;
+  form: FormGroup;
+  
   @ContentChild(IonInput) input: IonInput;
 
-  constructor(private route:Router) { }
+  constructor(private route:Router, private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      newbuddy: this.formBuilder.array([]),
+    });
+  }
 
   ngOnInit() {
     this.passwordToggleIcon = 'eye';
+    this.passwordToggleIcon2 = 'eye';
   }
 
-  toggleShow() {
-    this.showPassword = !this.showPassword;
-    if (this.passwordToggleIcon === 'eye') {
-          this.passwordToggleIcon = 'eye-off';
+  toggleShow(name) {
+    let btn = name.target.id;
+    if (btn ==="pass") {
+      this.passToggle();
     } else {
-      console.log('not eye');
-      this.passwordToggleIcon = 'eye';
+      this.confirmPassToggle();
     }
+  }
+
+  passToggle() {
+      this.showPassword = !this.showPassword;
+      if (this.passwordToggleIcon === 'eye') {
+            this.passwordToggleIcon = 'eye-off';
+      } else {
+        console.log('not eye');
+        this.passwordToggleIcon = 'eye';
+      }
+  }
+
+  confirmPassToggle() {
+      this.showConfPassword = !this.showConfPassword;
+      if (this.passwordToggleIcon2 === 'eye') {
+            this.passwordToggleIcon2 = 'eye-off';
+      } else {
+        console.log('not eye');
+        this.passwordToggleIcon2 = 'eye';
+      }    
+  }
+
+  addBuddy() {
+    console.log('new buddy')
+    const buddy = this.form.controls.newbuddy as FormArray;
+    buddy.push(
+      this.formBuilder.group({
+        name: '',
+        breed: '',
+      })
+    );
   }
 }
