2f4ceb6e495a3f9938bf155a50a6566f3e579e8e
Muffinous <mimiafsx@outlook.es>
2022-11-18 17:08:38 +0100

    buddy page popup - update info. progress

--- src/app/home/buddy/buddy.page.ts
+++ src/app/home/buddy/buddy.page.ts
@@ -1,9 +1,12 @@
 import { formatDate, getLocaleDateFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, ModalController, NavParams } from '@ionic/angular';
 import { Buddy } from 'src/app/models/buddy';
 import { AnimalService } from 'src/app/services/animal/animal.service';
 import { UserService } from 'src/app/services/auth/user/user.service';
+import { IonLoaderService } from 'src/app/services/ion-loader.service';
 
 @Component({
   selector: 'app-buddy',
@@ -14,8 +17,12 @@ export class BuddyPage implements OnInit {
   buddy = {} as Buddy
   bday
   editMode = false;
-
-  constructor(public modalControler: ModalController, public navParams: NavParams, private animalService: AnimalService, private alertCtrl: AlertController,
+  breedString;
+  public ionicForm: FormGroup;
+  image;
+  
+  constructor(public modalControler: ModalController, private afs: AngularFirestore, public navParams: NavParams, private animalService: AnimalService,  
+    private ionLoaderService: IonLoaderService, private formBuilder: FormBuilder, private alertCtrl: AlertController,
     private userService: UserService) { 
     this.buddy.buddyName = this.navParams.get('buddyName');
     this.buddy.buddyAge = this.navParams.get('buddyAge');
@@ -27,9 +34,32 @@ export class BuddyPage implements OnInit {
     this.buddy.buddyDescription = this.navParams.get('buddyDescription');
     this.buddy.id = this.navParams.get('id');
 
+    this.breedString = this.buddy.buddyBreed.toString();
+    
     this.bday = formatDate(this.buddy.buddyBday, 'dd-MM-yyyy', 'es-ES')
   
     console.log('buddypage', this.buddy, 'bday', this.bday)
+  }
+  
+  validation_messages = {
+    'edit_BuddyName': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'edit_buddyType': [
+      { type: 'required', message: 'Type is required.' }
+    ],
+    'edit_buddyGender': [
+      { type: 'required', message: 'Gender is required.' }
+    ],
+    'edit_buddyAge': [
+      { type: 'required', message: 'Age is required.' }
+    ], 
+    'edit_buddyBreed': [
+      { type: 'required', message: 'Breed is required.' }
+    ],
+    'edit_buddyBday': [
+      { type: 'required', message: 'Birthday is required.' }
+    ]
   }
   
   ngOnInit() {
@@ -71,9 +101,95 @@ export class BuddyPage implements OnInit {
 
   editBuddy() {
     this.editMode = true;
+    this.ionicForm = this.formBuilder.group({
+      id: [this.buddy.id],
+      edit_buddyName: ['', [Validators.minLength(2)]], 
+      edit_buddyGender: [''],
+      edit_buddyType: [''],
+      edit_buddyAge: [''],
+      edit_buddyBreed: [''],
+      edit_buddyBday: [''],   
+      edit_buddyPic: ['']
+    })
+  }
+
+  saveEdit() {
+    let buddyPreview = {} as Buddy
+    buddyPreview = this.buddy;
+
+    const userBuddiesRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${this.userService.user.username}/buddies/${this.buddy.buddyName}`
+      ); 
+      console.log('userBuddiesRef values ', userBuddiesRef)
+
+      console.log('Ionic form values ', this.ionicForm.value)
+      if (this.ionicForm.value.edit_buddyName) {
+        console.log("update buddy name ", this.buddy.buddyName ," to ", this.ionicForm.value.edit_buddyName)
+        buddyPreview.buddyName = this.ionicForm.value.edit_buddyName
+      }
+
+      if (this.ionicForm.value.edit_buddyGender) {
+        console.log("update buddy gender  ", this.buddy.buddyGender ," to ", this.ionicForm.value.edit_buddyGender)
+        buddyPreview.buddyGender = this.ionicForm.value.edit_buddyGender
+
+      }
+
+      if (this.ionicForm.value.edit_buddyType) {
+        console.log("update buddy type  ", this.buddy.buddyType ," to ", this.ionicForm.value.edit_buddyType)
+        buddyPreview.buddyType = this.ionicForm.value.edit_buddyType
+
+      }
+
+      if (this.ionicForm.value.edit_buddyBreed) {
+        console.log("update buddy breed  ", this.buddy.buddyBreed ," to ", this.ionicForm.value.edit_buddyBreed)
+        buddyPreview.buddyBreed = this.ionicForm.value.edit_buddyBreed
+
+      }
+
+      if (this.ionicForm.value.edit_buddyBday) {
+        console.log("update buddy bday  ", this.buddy.buddyBday ," to ", this.ionicForm.value.edit_buddyBday)
+        buddyPreview.buddyBday = this.ionicForm.value.edit_buddyBday
+
+      }
+      if (this.ionicForm.value.edit_BuddyDescription) {
+        console.log("update buddy description  ", this.buddy.buddyDescription ," to ", this.ionicForm.value.edit_BuddyDescription)
+        buddyPreview.buddyDescription = this.ionicForm.value.edit_BuddyDescription
+
+      }
+      // userBuddiesRef.update({buddyName: this.ionicForm.value.attributes.edit_buddyName}), {
+      //   merge: true
+      // }
+      let subheader : string = "You're gonna update your buddy's info. Is everything correct?"
+      let message : string = `<ul><li>  Buddy name : ${buddyPreview.buddyName}</li>
+                              <li>Buddy gender : ${buddyPreview.buddyGender}</li>
+                              <li>Buddy type : ${buddyPreview.buddyType}</li>
+                              <li>Buddy breed : ${buddyPreview.buddyBreed}</li>
+                              <li>Buddy birthday : ${formatDate(buddyPreview.buddyBday, 'dd-MM-yyyy', 'es-ES')}</li>
+                              <li>Buddy description : ${buddyPreview.buddyDescription}</li></ul>`
+
+      this.ionLoaderService.presentAlert(message, "Update Buddy Info", subheader)
   }
 
   cancelEdit() {
     this.editMode = false;
+  }
+
+  changeBreed(typeofAnimal) {
+    console.log("change breed before console.log typeofanimal")
+    console.log('value : ', typeofAnimal)
+    this.animalService.setActualBreed(typeofAnimal)
+  }
+
+  
+  onFileSelected(event, id) {
+    const file = event.target.files[0]; // info de la imagen
+    let reader = new FileReader();
+
+    reader.onload = (e: any) => {
+        this.buddy.buddyPic = e.target.result
+    }
+    reader.readAsDataURL(file) // para que aparezca la imagen en la pantalla
+
+    //this.uploadImage(event, id)
   }
 }
